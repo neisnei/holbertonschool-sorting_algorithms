391f0ba68e8437a04a6c965d0a1283c3986a5358
neisnei <neischalyruidiaz@gmail.com>
2023-09-14 17:30:06 +0000

    README.md

---
+++ 0-bubble_sort.c
@@ -0,0 +1,11 @@
+#include "sort.h"
+
+/*
+ * bubble_sort - compare pairs of adjacent elements and swap if in wrong order
+ * @array: stores elements of the same type
+ * @size_t: represent memory sizes or array indices
+ * @size: size or length of a data structure
+ * Return: void
+
+void bubble_sort(int *array, size_t size)
+{
