0012ad08fbc2a2ca4fed2d6d3affe59cdd4b5319
neisnei <130294790+neisnei@users.noreply.github.com>
2023-09-18 20:31:49 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,7 +3,6 @@
 /*
  * bubble_sort - compare pairs of adjacent elements and swap if in wrong order
  * @array: stores elements of the same type
- * @size_t: represent memory sizes or array indices
  * @size: size or length of a data structure
  * Return: Always 0
  */
