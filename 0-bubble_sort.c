fa0135ba831f5266ede2983456a7bc9194ef3b7a
neisnei <130294790+neisnei@users.noreply.github.com>
2023-09-18 19:38:02 +0200

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,25 +8,3 @@
  * Return: Always 0
  */
 
-void bubble_sort(int *array, size_t size)
-{
-int tmp;
-	size_t i, j;
-
-	if (array == NULL || size == 0)
-		return;
-
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				tmp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = tmp;
-				print_array(array, size);
-			}
-		}
-	}
-}
