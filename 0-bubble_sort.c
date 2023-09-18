fc88adda4e802af3d348d893854bc15c7107dade
neisnei <neischalyruidiaz@gmail.com>
2023-09-18 18:02:50 +0000

    README.md

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -11,12 +11,12 @@
 void bubble_sort(int *array, size_t size)
 {
 	int tmp;
-	size_t a, j;
+	size_t i, j;
 
 	if (array == NULL || size == 0)
 		return;
 
-	for (a = 0; a < size; a++)
+	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size - 1; j++)
 		{
