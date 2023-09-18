ad4ada910c354972023b139bc3b0f8826af6702a
neisnei <neischalyruidiaz@gmail.com>
2023-09-18 17:33:37 +0000

    README.md

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -5,9 +5,28 @@
  * @array: stores elements of the same type
  * @size_t: represent memory sizes or array indices
  * @size: size or length of a data structure
- * Return: void
+ * Return: Always 0
  */
 
 void bubble_sort(int *array, size_t size)
 {
+	int tmp;
+	size_t a, k;
 
+	if (array == NULL || size == 0)
+		return;
+
+	for (a = 0; a < size; a++)
+	{
+		for (k = 0; k < size - 1; k++)
+		{
+			if (array[k] > array[k + 1])
+			{
+				tmp = array[k];
+				array[k] = array[k + 1];
+				array[k + 1] = tmp;
+				print_array(array, size);
+			}
+		}
+	}
+}
